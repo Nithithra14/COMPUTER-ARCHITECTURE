7b3faabfc0c163cb6d7dd15f16aebb982d84168a
Nithithra14 <120041256+Nithithra14@users.noreply.github.com>
2024-03-18 08:21:39 +0530

    Add files via upload

---
+++ cpu time.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("Enter the cpu clock cycle time:\n");
+	scanf("%d",&a);
+	printf("Entre the clock cycle time:");
+	scanf("%d",&b);
+	printf("The cpu time:%d",a*b);
+	return 0;
+}
